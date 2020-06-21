049a53a531931d736cb162048cb3b6f933317af0
dophukhuong <55820346+dophukhuong@users.noreply.github.com>
2020-06-21 19:01:48 +0700

    Update Fragment_Hot_Songs.java
    Fragment_HotSong

--- app/src/main/java/com/example/musicappth73nhom16/Fragment/Fragment_Hot_Songs.java
+++ app/src/main/java/com/example/musicappth73nhom16/Fragment/Fragment_Hot_Songs.java
@@ -1,6 +1,63 @@
 package com.example.musicappth73nhom16.Fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.musicappth73nhom16.Adapter.BaiHatHotAdapter;
+import com.example.musicappth73nhom16.Adapter.BannerAdapter;
+import com.example.musicappth73nhom16.Model.BaiHat;
+import com.example.musicappth73nhom16.R;
+import com.example.musicappth73nhom16.Service.APIService;
+import com.example.musicappth73nhom16.Service.DataService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Fragment_Hot_Songs extends Fragment {
+	 View view;
+    RecyclerView recyclerViewBaiHatHot;
+    BaiHatHotAdapter baiHatHotAdapter;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_baihat_thichnhat,container,false);
+        recyclerViewBaiHatHot = view.findViewById(R.id.recyclerviewbaihathot);
+        GetData();
+
+        return view;
+    }
+
+    private void GetData() {
+        DataService dataService= APIService.getService();
+        Call<List<BaiHat>> callback = dataService.GetBaiHatHot();
+        callback.enqueue(new Callback<List<BaiHat>>() {
+            @Override
+            public void onResponse(Call<List<BaiHat>> call, Response<List<BaiHat>> response) {
+                ArrayList<BaiHat> baiHatArrayList = (ArrayList<BaiHat>) response.body();
+                baiHatHotAdapter = new BaiHatHotAdapter(getActivity(),baiHatArrayList);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+                linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                recyclerViewBaiHatHot.setLayoutManager(linearLayoutManager);
+                recyclerViewBaiHatHot.setAdapter(baiHatHotAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<BaiHat>> call, Throwable t) {
+
+            }
+        });
+
+    }
 }
