6b6a6d6c1d36a3af1cd632f037d8d48687837557
dophukhuong <55820346+dophukhuong@users.noreply.github.com>
2020-06-21 19:01:37 +0700

    Update Fragment_Album_Hot.java
    Fragment_AlbumHot

--- app/src/main/java/com/example/musicappth73nhom16/Fragment/Fragment_Album_Hot.java
+++ app/src/main/java/com/example/musicappth73nhom16/Fragment/Fragment_Album_Hot.java
@@ -1,6 +1,75 @@
 package com.example.musicappth73nhom16.Fragment;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.musicappth73nhom16.Activity.DanhSachTatCaAlbumActivity;
+import com.example.musicappth73nhom16.Adapter.AlbumAdapter;
+import com.example.musicappth73nhom16.Model.Album;
+import com.example.musicappth73nhom16.R;
+import com.example.musicappth73nhom16.Service.APIService;
+import com.example.musicappth73nhom16.Service.DataService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Fragment_Album_Hot extends Fragment {
+ 	View view;
+    RecyclerView recyclerViewAlbum;
+    TextView txtXemThemAlbum;
+    AlbumAdapter albumAdapter;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_album_hot,container,false);
+        recyclerViewAlbum = view.findViewById(R.id.recyclerviewalbum);
+        txtXemThemAlbum = view.findViewById(R.id.textviewxemthemalbum);
+        txtXemThemAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //dùng để chuyển màn hình khi click vô chữ xem thêm
+                Intent intent = new Intent(getActivity(), DanhSachTatCaAlbumActivity.class);
+                startActivity(intent);
+            }
+        });
+        GetData();
+        return view;
+    }
+
+    private void GetData() {
+        DataService dataService = APIService.getService();
+        Call<List<Album>> callback = dataService.GetAlbumHot();
+        callback.enqueue(new Callback<List<Album>>() {
+            @Override
+            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                ArrayList<Album> albumArrayList = (ArrayList<Album>) response.body();
+                albumAdapter = new AlbumAdapter(getActivity(), albumArrayList);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+                linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+                recyclerViewAlbum.setLayoutManager(linearLayoutManager);
+                recyclerViewAlbum.setAdapter(albumAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Album>> call, Throwable t) {
+
+            }
+        });
+    }
 }
