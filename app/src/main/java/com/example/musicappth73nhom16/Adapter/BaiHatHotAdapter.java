069858f5f2187d4f4257749cd34cef9f2892037b
dophukhuong <55820346+dophukhuong@users.noreply.github.com>
2020-06-21 19:01:23 +0700

    Create BaiHatHotAdapter.java
    BaiHatHotAdapter

---
+++ app/src/main/java/com/example/musicappth73nhom16/Adapter/BaiHatHotAdapter.java
@@ -0,0 +1,103 @@
+package com.example.musicappth73nhom16.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.musicappth73nhom16.Activity.PlayNhacActivity;
+import com.example.musicappth73nhom16.Model.BaiHat;
+import com.example.musicappth73nhom16.R;
+import com.example.musicappth73nhom16.Service.APIService;
+import com.example.musicappth73nhom16.Service.DataService;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class BaiHatHotAdapter extends RecyclerView.Adapter<BaiHatHotAdapter.ViewHolder> {
+    Context context;
+    ArrayList<BaiHat> mangBaiHatHot;
+
+    public BaiHatHotAdapter(Context context, ArrayList<BaiHat> baiHatArrayList) {
+        this.context = context;
+        this.mangBaiHatHot = baiHatArrayList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.dong_baihat_hot,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        BaiHat baiHat = mangBaiHatHot.get(position);
+        holder.txtCaSi.setText(baiHat.getCaSi());
+        holder.txtTen.setText(baiHat.getTenBaiHat());
+        Picasso.with(context).load(baiHat.getHinhBaiHat()).into(holder.imgHinh);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mangBaiHatHot.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView txtTen,txtCaSi;
+        ImageView imgHinh,imgLuotThich;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtTen = itemView.findViewById(R.id.textviewtenbaihathot);
+            txtCaSi = itemView.findViewById(R.id.textviewtencasibaihathot);
+            imgHinh  = itemView.findViewById(R.id.imageviewbaihathot);
+            imgLuotThich = itemView.findViewById(R.id.imageviewluotthich);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, PlayNhacActivity.class);
+                    intent.putExtra("cakhuc",mangBaiHatHot.get(getPosition()));
+                    context.startActivity(intent);
+                }
+            });
+            imgLuotThich.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    imgLuotThich.setImageResource(R.drawable.iconloved);
+                    DataService dataService = APIService.getService();
+                    Call<String> callback = dataService.UpdateLuotThich("1",mangBaiHatHot.get(getPosition()).getIdBaiHat());
+                    callback.enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            String ketqua = response.body();
+                            if (ketqua.equals("Ok")){
+                                Toast.makeText(context, "Đã thích", Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(context, "Lỗi !!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+
+                        }
+                    });
+                    imgLuotThich.setEnabled(false);
+                }
+            });
+        }
+    }
+}
